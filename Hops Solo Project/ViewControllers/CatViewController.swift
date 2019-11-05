ccc1f4981807f224b1f45ec3200bd36e33649671
karyRmartinez <karymartinez@pursuit.org>
2019-11-05 10:41:16 -0500

    first UIView for category sections

--- Hops Solo Project/ViewControllers/CatViewController.swift
+++ Hops Solo Project/ViewControllers/CatViewController.swift
@@ -12,5 +12,36 @@ import UIKit
 class CatViewController: UIViewController {
     
     
+    lazy var myView: UIView = { [unowned self] in
+         let view = UIView()
+         view.backgroundColor = .green
+         view.layer.cornerRadius = view.frame.height / 2
+        view.translatesAutoresizingMaskIntoConstraints = false
+         return view
+     }()
+     
 
+
+    private func setViewOneConstraints() {
+         NSLayoutConstraint.activate([
+                    myView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                   myView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -250),
+                 myView.widthAnchor.constraint(equalToConstant: 100),
+                  myView.heightAnchor.constraint(equalToConstant: 100)
+                 ])
+          }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setViewOneConstraints()
+
+    }
+
+  private func addSubview() {
+    self.view.addSubview(myView)
+       
+    }
 }
