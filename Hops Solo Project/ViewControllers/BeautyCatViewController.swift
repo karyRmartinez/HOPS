fbf0ecf60c3487e014ee1ebd6c783910986467b8
karyRmartinez <karymartinez@pursuit.org>
2019-11-26 15:04:33 -0500

    collectionView for my Beauty catagory

--- Hops Solo Project/ViewControllers/BeautyCatViewController.swift
+++ Hops Solo Project/ViewControllers/BeautyCatViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class BeautyCatViewController: UIViewController {
+    
+    var Makeup = [VeganMakeUpElement]() {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    
 
     lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -17,8 +24,8 @@ class BeautyCatViewController: UIViewController {
         let BeautyView = UICollectionView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), collectionViewLayout: layout)
         
         BeautyView .register(BeautyCollectionViewCell.self, forCellWithReuseIdentifier: "theCell")
-//        BeautyView .dataSource = self
-//        BeautyView .delegate = self
+        BeautyView .dataSource = self
+        BeautyView .delegate = self
         BeautyView.backgroundColor = .white
         
         
@@ -27,23 +34,51 @@ class BeautyCatViewController: UIViewController {
         
     }()
     
+    private func loadData() {
+        MakeupAPIClient.manager.getElements { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let MakeupFromOnline):
+                    self.Makeup = MakeupFromOnline
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        loadData()
+        
+      
     }
 
 
 }
-//
-//extension BeautyCatViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        <#code#>
-//    }
-//
-//
-//}
+
+extension BeautyCatViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Makeup.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "theCell", for: indexPath) as! BeautyCollectionViewCell
+        let currentMU = Makeup[indexPath.row]
+        cell.MakeupLabel.text = currentMU.brand
+        return cell
+        
+    }
+
+
+}
+
+extension BeautyCatViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
--- Hops Solo Project/ViewControllers/BeautyCollectionViewCell.swift
+++ Hops Solo Project/ViewControllers/BeautyCollectionViewCell.swift
@@ -10,4 +10,64 @@ import UIKit
 
 class BeautyCollectionViewCell: UICollectionViewCell {
     
+    lazy var imageView: UIImageView = {
+        let  Image = UIImageView()
+        Image.translatesAutoresizingMaskIntoConstraints = false
+        return Image
+    }()
+    
+    
+    lazy var MakeupLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        return label
+    }()
+    
+    func setImage() {
+        NSLayoutConstraint.activate([
+            
+            imageView.centerXAnchor.constraint(equalTo: imageView.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: imageView.centerYAnchor, constant: -250),
+            imageView.widthAnchor.constraint(equalToConstant: 100),
+            imageView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
+    }
+    
+    
+    func setlabel() {
+        NSLayoutConstraint.activate([
+            
+           MakeupLabel.centerXAnchor.constraint(equalTo: MakeupLabel.centerXAnchor),
+            MakeupLabel.centerYAnchor.constraint(equalTo: imageView.centerYAnchor, constant: -250),
+            MakeupLabel.widthAnchor.constraint(equalToConstant: 100),
+           MakeupLabel.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
+    }
+    
+    
+  
+    
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .orange
+        setImage()
+        setlabel()
+        
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
+
+
+
+
+
+
+
