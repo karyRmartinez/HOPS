8a4ea306d591f447d574f57419f613120d77157d
karyRmartinez <karymartinez@pursuit.org>
2019-11-26 12:02:59 -0500

    Image Helper

---
+++ Hops Solo Project/Networking /Imagehelper.swift
@@ -0,0 +1,48 @@
+//
+//  Imagehelper.swift
+//  Hops Solo Project
+//
+//  Created by Kary Martinez on 11/26/19.
+//  Copyright © 2019 Kary Martinez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ImageHelper {
+    private init() {}
+    
+    // MARK: - Static Properties
+    static let shared = ImageHelper()
+    
+    // MARK: - Internal Methods
+    func getImage(urlStr: String, completionHandler: @escaping (Result<UIImage,AppError>) -> ()) {
+        
+        guard let url = URL(string: urlStr) else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            guard error == nil else {
+                completionHandler(.failure(.noDataReceived))
+                return
+            }
+            
+            guard let data = data else {
+                completionHandler(.failure(.noDataReceived))
+                return
+            }
+            
+            guard let image = UIImage(data: data) else {
+                completionHandler(.failure(.notAnImage))
+                return
+            }
+            
+            completionHandler(.success(image))
+            
+            } .resume()
+        
+    }
+    
+}
+
